95c8a8989034cdd1946d5cbc8331e88f98277730
shobhitsingh29 <shobhitsingh29@gmail.com>
2021-05-01 23:10:45 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # shubs
 shubs next app
+shubs 
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,15 @@
+import React from "react";
+import App from "next/app";
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/antd.less";
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return <Component {...pageProps} />;
+  }
 }
 
-export default MyApp
+export default MyApp;
