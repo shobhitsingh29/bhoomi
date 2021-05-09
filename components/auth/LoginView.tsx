9f3da6bb0bdd5b42b1072fb5ea92b19f4471d1c1
shobsing <shobsing@adobe.com>
2021-05-09 11:38:48 +0530

    k

--- components/auth/LoginView.tsx
+++ components/auth/LoginView.tsx
@@ -48,7 +48,7 @@ const LoginView: FC<any> = () => {
     handleValidation();
   }, [handleValidation]);
 
-   const onSignIn=(googleUser)=> {
+   const onSignIn=(googleUser:any)=> {
      const profile = googleUser.getBasicProfile()
      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
