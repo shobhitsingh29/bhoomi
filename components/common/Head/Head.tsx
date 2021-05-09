7a05ac0001835529b31056cc712fb557744e5cbe
shobsing <shobsing@adobe.com>
2021-05-09 11:45:47 +0530

    k

--- components/common/Head/Head.tsx
+++ components/common/Head/Head.tsx
@@ -10,11 +10,12 @@ const Head: FC = () => {
             <NextHead>
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <link rel='manifest' href='/site.webmanifest' key='site-manifest' />
-                <meta name="google-signin-client_id" content="94873425938-1ot94c7vm2adr02klj5cefijjvjemen2.apps.googleusercontent.com"/>
                 <script src='https://apis.google.com/js/platform.js' />
+                <meta name="google-signin-client_id" content="94873425938-1ot94c7vm2adr02klj5cefijjvjemen2.apps.googleusercontent.com"/>
             </NextHead>
         </>
     )
 }
 
 export default Head
+̊
