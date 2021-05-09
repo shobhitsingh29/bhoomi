4f0fc7cd1f1b9046a905392a3e1536c44ebc1295
shobhitsingh29 <shobhitsingh29@gmail.com>
2021-05-09 00:12:29 +0000

    asd

--- components/ui/context.tsx
+++ components/ui/context.tsx
@@ -1,174 +1,174 @@
-import React, { FC, useMemo } from 'react'
-import { ThemeProvider } from 'next-themes'
+import React, { FC, useMemo } from "react";
+import { ThemeProvider } from "next-themes";
 
 export interface State {
-  displaySidebar: boolean
-  displayDropdown: boolean
-  displayModal: boolean
-  displayToast: boolean
-  modalView: string
-  toastText: string
-  userAvatar: string
+  displaySidebar: boolean;
+  displayDropdown: boolean;
+  displayModal: boolean;
+  displayToast: boolean;
+  modalView: string;
+  toastText: string;
+  userAvatar: string;
 }
 
 const initialState = {
   displaySidebar: false,
   displayDropdown: false,
-  displayModal: false,
-  modalView: 'LOGIN_VIEW',
+  displayModal: true,
+  modalView: "LOGIN_VIEW",
   displayToast: false,
-  toastText: '',
-  userAvatar: '',
-}
+  toastText: "",
+  userAvatar: "",
+};
 
 type Action =
   | {
-      type: 'OPEN_SIDEBAR'
+      type: "OPEN_SIDEBAR";
     }
   | {
-      type: 'CLOSE_SIDEBAR'
+      type: "CLOSE_SIDEBAR";
     }
   | {
-      type: 'OPEN_TOAST'
+      type: "OPEN_TOAST";
     }
   | {
-      type: 'CLOSE_TOAST'
+      type: "CLOSE_TOAST";
     }
   | {
-      type: 'SET_TOAST_TEXT'
-      text: ToastText
+      type: "SET_TOAST_TEXT";
+      text: ToastText;
     }
   | {
-      type: 'OPEN_DROPDOWN'
+      type: "OPEN_DROPDOWN";
     }
   | {
-      type: 'CLOSE_DROPDOWN'
+      type: "CLOSE_DROPDOWN";
     }
   | {
-      type: 'OPEN_MODAL'
+      type: "OPEN_MODAL";
     }
   | {
-      type: 'CLOSE_MODAL'
+      type: "CLOSE_MODAL";
     }
   | {
-      type: 'SET_MODAL_VIEW'
-      view: MODAL_VIEWS
+      type: "SET_MODAL_VIEW";
+      view: MODAL_VIEWS;
     }
   | {
-      type: 'SET_USER_AVATAR'
-      value: string
-    }
+      type: "SET_USER_AVATAR";
+      value: string;
+    };
 
 type MODAL_VIEWS =
-  | 'SIGNUP_VIEW'
-  | 'LOGIN_VIEW'
-  | 'FORGOT_VIEW'
-  | 'NEW_SHIPPING_ADDRESS'
-  | 'NEW_PAYMENT_METHOD'
-type ToastText = string
+  | "SIGNUP_VIEW"
+  | "LOGIN_VIEW"
+  | "FORGOT_VIEW"
+  | "NEW_SHIPPING_ADDRESS"
+  | "NEW_PAYMENT_METHOD";
+type ToastText = string;
 
-export const UIContext = React.createContext<State | any>(initialState)
+export const UIContext = React.createContext<State | any>(initialState);
 
-UIContext.displayName = 'UIContext'
+UIContext.displayName = "UIContext";
 
 function uiReducer(state: State, action: Action) {
   switch (action.type) {
-    case 'OPEN_SIDEBAR': {
+    case "OPEN_SIDEBAR": {
       return {
         ...state,
         displaySidebar: true,
-      }
+      };
     }
-    case 'CLOSE_SIDEBAR': {
+    case "CLOSE_SIDEBAR": {
       return {
         ...state,
         displaySidebar: false,
-      }
+      };
     }
-    case 'OPEN_DROPDOWN': {
+    case "OPEN_DROPDOWN": {
       return {
         ...state,
         displayDropdown: true,
-      }
+      };
     }
-    case 'CLOSE_DROPDOWN': {
+    case "CLOSE_DROPDOWN": {
       return {
         ...state,
         displayDropdown: false,
-      }
+      };
     }
-    case 'OPEN_MODAL': {
+    case "OPEN_MODAL": {
       return {
         ...state,
         displayModal: true,
         displaySidebar: false,
-      }
+      };
     }
-    case 'CLOSE_MODAL': {
+    case "CLOSE_MODAL": {
       return {
         ...state,
         displayModal: false,
-      }
+      };
     }
-    case 'OPEN_TOAST': {
+    case "OPEN_TOAST": {
       return {
         ...state,
         displayToast: true,
-      }
+      };
     }
-    case 'CLOSE_TOAST': {
+    case "CLOSE_TOAST": {
       return {
         ...state,
         displayToast: false,
-      }
+      };
     }
-    case 'SET_MODAL_VIEW': {
+    case "SET_MODAL_VIEW": {
       return {
         ...state,
         modalView: action.view,
-      }
+      };
     }
-    case 'SET_TOAST_TEXT': {
+    case "SET_TOAST_TEXT": {
       return {
         ...state,
         toastText: action.text,
-      }
+      };
     }
-    case 'SET_USER_AVATAR': {
+    case "SET_USER_AVATAR": {
       return {
         ...state,
         userAvatar: action.value,
-      }
+      };
     }
   }
 }
 
 export const UIProvider: FC = (props) => {
-  const [state, dispatch] = React.useReducer(uiReducer, initialState)
+  const [state, dispatch] = React.useReducer(uiReducer, initialState);
 
-  const openSidebar = () => dispatch({ type: 'OPEN_SIDEBAR' })
-  const closeSidebar = () => dispatch({ type: 'CLOSE_SIDEBAR' })
+  const openSidebar = () => dispatch({ type: "OPEN_SIDEBAR" });
+  const closeSidebar = () => dispatch({ type: "CLOSE_SIDEBAR" });
   const toggleSidebar = () =>
     state.displaySidebar
-      ? dispatch({ type: 'CLOSE_SIDEBAR' })
-      : dispatch({ type: 'OPEN_SIDEBAR' })
+      ? dispatch({ type: "CLOSE_SIDEBAR" })
+      : dispatch({ type: "OPEN_SIDEBAR" });
   const closeSidebarIfPresent = () =>
-    state.displaySidebar && dispatch({ type: 'CLOSE_SIDEBAR' })
+    state.displaySidebar && dispatch({ type: "CLOSE_SIDEBAR" });
 
-  const openDropdown = () => dispatch({ type: 'OPEN_DROPDOWN' })
-  const closeDropdown = () => dispatch({ type: 'CLOSE_DROPDOWN' })
+  const openDropdown = () => dispatch({ type: "OPEN_DROPDOWN" });
+  const closeDropdown = () => dispatch({ type: "CLOSE_DROPDOWN" });
 
-  const openModal = () => dispatch({ type: 'OPEN_MODAL' })
-  const closeModal = () => dispatch({ type: 'CLOSE_MODAL' })
+  const openModal = () => dispatch({ type: "OPEN_MODAL" });
+  const closeModal = () => dispatch({ type: "CLOSE_MODAL" });
 
-  const openToast = () => dispatch({ type: 'OPEN_TOAST' })
-  const closeToast = () => dispatch({ type: 'CLOSE_TOAST' })
+  const openToast = () => dispatch({ type: "OPEN_TOAST" });
+  const closeToast = () => dispatch({ type: "CLOSE_TOAST" });
 
   const setUserAvatar = (value: string) =>
-    dispatch({ type: 'SET_USER_AVATAR', value })
+    dispatch({ type: "SET_USER_AVATAR", value });
 
   const setModalView = (view: MODAL_VIEWS) =>
-    dispatch({ type: 'SET_MODAL_VIEW', view })
+    dispatch({ type: "SET_MODAL_VIEW", view });
 
   const value = useMemo(
     () => ({
@@ -187,21 +187,21 @@ export const UIProvider: FC = (props) => {
       setUserAvatar,
     }),
     [state]
-  )
+  );
 
-  return <UIContext.Provider value={value} {...props} />
-}
+  return <UIContext.Provider value={value} {...props} />;
+};
 
 export const useUI = () => {
-  const context = React.useContext(UIContext)
+  const context = React.useContext(UIContext);
   if (context === undefined) {
-    throw new Error(`useUI must be used within a UIProvider`)
+    throw new Error(`useUI must be used within a UIProvider`);
   }
-  return context
-}
+  return context;
+};
 
 export const ManagedUIContext: FC = ({ children }) => (
   <UIProvider>
     <ThemeProvider>{children}</ThemeProvider>
   </UIProvider>
-)
+);
