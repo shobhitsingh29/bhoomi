e01837b18bb0e62c0e257b99c246e68228cb52db
shobsing <shobsing@adobe.com>
2021-05-15 08:30:48 +0530

    s

--- components/ui/UserDetails/UserDetails.tsx
+++ components/ui/UserDetails/UserDetails.tsx
@@ -1,33 +1,31 @@
-import React, { FC } from "react";
-import styles from "./UserDetails.module.css";
+import React, { FC } from 'react'
+import styles from './UserDetails.module.css'
 import { useSession } from 'next-auth/client'
-import Image from "next/image";
 
 const UserDetails: FC<any> = () => {
-  const [session, loading] = useSession();
+    const [session, loading] = useSession()
 
-  return (
-      <main className={styles.main}>
-        <div className={styles.user}>
-          {loading && <div className={styles.title}>Loading...</div>}
-          {session && <> <p style={{ marginBottom: '10px' }}> Welcome, {session?.user?.name ?? session?.user?.email}</p> <br />
-              <Image
-                  quality="85"
-                  src={session?.user?.image ?? ""} alt="" className={styles.avatar}
-                  height={64}
-                  width={64}
-                  layout="fixed"
-              />
-          </>}
-          {!session &&
-          <>
-            <p className={styles.title}>Please Sign in</p>
-            <img src="https://cdn.dribbble.com/users/759083/screenshots/6915953/2.gif" alt="" className={styles.avatar} />
-          </>
-          }
-        </div>
-      </main>
-  );
-};
+    return (
+        <main className={styles.main}>
+            <div className={styles.user}>
+                {loading && <div className={styles.title}>Loading...</div>}
+                {session && <> <p
+                    style={{ marginBottom: '10px' }}> Welcome, {session?.user?.name ?? session?.user?.email}</p> <br />
+                    <img
+                        src={session?.user?.image ?? ''}
+                        alt='user image'
+                    />
+                </>}
+                {!session &&
+                <>
+                    <p className={styles.title}>Please Sign in</p>
+                    <img src='https://cdn.dribbble.com/users/759083/screenshots/6915953/2.gif' alt=''
+                         className={styles.avatar} />
+                </>
+                }
+            </div>
+        </main>
+    )
+}
 
-export default UserDetails;
+export default UserDetails
