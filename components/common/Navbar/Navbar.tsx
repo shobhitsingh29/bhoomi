f21ecfe87b78013e9e548ee024c3bd531e4aa12f
shobsing <shobsing@adobe.com>
2021-05-15 17:47:59 +0530

    s

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -1,76 +1,79 @@
-import { FC } from "react";
-import Link from "next/link";
-import { Logo, Container } from "@components/ui";
-import { Searchbar, UserNav } from "@components/common";
-import NavbarRoot from "./NavbarRoot";
-import s from "./Navbar.module.css";
+import { FC } from 'react'
+import Link from 'next/link'
+import { Logo, Container } from '@components/ui'
+import { Searchbar, UserNav } from '@components/common'
+import NavbarRoot from './NavbarRoot'
+import s from './Navbar.module.css'
 import cn from 'classnames'
+import { useSession } from 'next-auth/client'
 
-const Navbar: FC = () => (
-  <NavbarRoot>
-    <Container>
-      <div className=" relative flex flex-row justify-between py-4 align-center md:py-6 flex-wrap">
-        <div className="flex items-centerflex items-center ">
-          <Link href="/">
-            <a aria-label="Logo" className={cn(
-                s.logo
-            )}>
-              <Logo />
-            </a>
-          </Link>
-            <nav className="hidden ml-6 space-x-4 lg:block">
-                <Link href="/">
-                    <a className={s.link}>Home</a>
-                </Link>
-                <Link href="/labs">
-                    <a className={s.link}>Labs</a>
-                </Link>
-                <Link href="/blog">
-                    <a className={s.link}>Blog</a>
-                </Link>
-                <Link href="/faq">
-                    <a className={s.link}>FAQ</a>
-                </Link>
-            </nav>
+const Navbar: FC = () => {
+    const [session] = useSession()
 
-            <nav   className={cn(
-                "lg:hidden flex fixed",
-                s.mobilebarContainer,
-            )}>
-                <Link href="/">
-                    <a className={s.mobilebarTabItem}>Home</a>
-                </Link>
-                <Link href="/labs">
-                    <a className={s.mobilebarTabItem}>Labs</a>
-                </Link>
-                <Link href="/blog">
-                    <a className={s.mobilebarTabItem}>Blog</a>
-                </Link>
-                <Link href="/faq">
-                    <a className={s.mobilebarTabItem}>FAQ</a>
-                </Link>
-            </nav>
-        </div>
+    return (<NavbarRoot>
+        <Container>
+            <div className=' relative flex flex-row justify-between py-4 align-center md:py-6 flex-wrap'>
+                <div className='flex items-centerflex items-center '>
+                    <Link href='/'>
+                        <a aria-label='Logo' className={cn(
+                            s.logo,
+                        )}>
+                            <Logo />
+                        </a>
+                    </Link>
+                    <nav className='hidden ml-6 space-x-4 lg:block'>
+                        <Link href='/'>
+                            <a className={s.link}>Home</a>
+                        </Link>
+                        {session && <Link href='/labs'>
+                            <a className={s.link}>Labs</a>
+                        </Link>}
+                        <Link href='/blog'>
+                            <a className={s.link}>Blog</a>
+                        </Link>
+                        <Link href='/faq'>
+                            <a className={s.link}>FAQ</a>
+                        </Link>
+                    </nav>
 
-        <div
-            className={cn(
-                "justify-center flex-1 hidden lg:flex lg:ml-72"
-            )}>
-          <Searchbar />
-        </div>
+                    <nav className={cn(
+                        'lg:hidden flex fixed',
+                        s.mobilebarContainer,
+                    )}>
+                        <Link href='/'>
+                            <a className={s.mobilebarTabItem}>Home</a>
+                        </Link>
+                        {session && <Link href='/labs'>
+                            <a className={s.mobilebarTabItem}>Labs</a>
+                        </Link>}
+                        <Link href='/blog'>
+                            <a className={s.mobilebarTabItem}>Blog</a>
+                        </Link>
+                        <Link href='/faq'>
+                            <a className={s.mobilebarTabItem}>FAQ</a>
+                        </Link>
+                    </nav>
+                </div>
 
-        <div className="flex justify-end flex-1 space-x-8">
-          <UserNav />
-        </div>
-      </div>
+                {session && <div
+                    className={cn(
+                        'justify-center flex-1 hidden lg:flex lg:ml-72',
+                    )}>
+                    <Searchbar />
+                </div>
+                }
+
+                <div className='flex justify-end flex-1 space-x-8'>
+                    <UserNav />
+                </div>
+            </div>
 
 
+            <div className='flex pb-4 lg:px-6 lg:hidden'>
+                <Searchbar id='mobile-search' />
+            </div>
+        </Container>
+    </NavbarRoot>)
+}
 
-      <div className="flex pb-4 lg:px-6 lg:hidden">
-        <Searchbar id="mobile-search" />
-      </div>
-    </Container>
-  </NavbarRoot>
-);
-
-export default Navbar;
+export default Navbar
--- pages/search/[id].tsx
+++ pages/search/[id].tsx
@@ -1,12 +1,29 @@
 import { useRouter } from 'next/router'
+import { Container, Text } from '@components/ui'
+import { Heart } from '@components/icons'
+import { Layout } from '@components/common'
 
-const SearchId: React.FC = () => {
+export default function SearchId() {
     const router = useRouter()
     const { id } = router.query
     return (
-        <>
-            <p>Post: {id}</p>
-        </>
+        <Container>
+            <div className='mt-3 mb-20'>
+                <Text variant='pageHeading'>Sample Id : {id}</Text>
+                <div className='group flex flex-col'>
+                    <div className='flex-1 px-12 py-24 flex flex-col justify-center items-center '>
+              <span
+                  className='border border-dashed border-secondary flex items-center justify-center w-16 h-16 bg-primary p-12 rounded-lg text-primary'>
+                <Heart className='absolute' />
+              </span>
+                        <h2 className='pt-6 text-2xl font-bold tracking-wide text-center'>
+                            Your Search is coming soon
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </Container>
     )
 }
-export default SearchId
+
+SearchId.Layout = Layout;
